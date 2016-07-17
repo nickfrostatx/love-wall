e0ae389ab018a5683e105590b15be726e19129ee
Nick Frost <nicholasfrost@khanacademy.org>
2016-07-16 17:24:16 -0700

    Override flask.send_static_file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Love Wall"""
 import flask
+import werkzeug.exceptions
 
 
 bp = flask.Blueprint('app', __name__)
@@ -11,8 +12,29 @@ def home():
     return flask.render_template('home.html')
 
 
+class App(flask.Flask):
+
+    def send_static_file(self, filename):
+        """Send static file, optionally using a compressed version."""
+        accept_encoding = flask.request.headers.get('Accept-Encoding', '')
+        cache_timeout = self.get_send_file_max_age(filename)
+        if 'gzip' in accept_encoding.lower():
+            try:
+                response = flask.send_from_directory(
+                    self.static_folder,
+                    filename + '.gz',
+                    cache_timeout=cache_timeout,
+                )
+                response.headers['Content-Encoding'] = 'gzip'
+                return response
+            except werkzeug.exceptions.NotFound:
+                pass
+        return flask.send_from_directory(self.static_folder, filename,
+                                         cache_timeout=cache_timeout)
+
+
 def create_app():
-    app = flask.Flask(__name__)
+    app = App(__name__)
     app.register_blueprint(bp)
     return app
 

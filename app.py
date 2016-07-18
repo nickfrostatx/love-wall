d10808495417edc761a8ba3184a0a4d50159e1ce
Nick Frost <nicholasfrost@khanacademy.org>
2016-07-17 22:51:41 -0700

    Add hearts

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 """Love Wall"""
+import base64
 import datetime
+import functools
+import os
+
 import flask
 import flask_sqlalchemy
+import sqlalchemy.sql
 import werkzeug.exceptions
 
 
@@ -15,17 +20,57 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String())
-    location_name = db.Column(db.String())
-    latitude = db.Column(db.Float())
-    longitude = db.Column(db.Float())
-    date = db.Column(db.Date())
-    description = db.Column(db.Text())
+    event_name = db.Column(db.String)
+    location_name = db.Column(db.String)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    date = db.Column(db.Date)
+    description = db.Column(db.Text)
+
+    def get_score(self):
+        return self.votes.count()
+
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Heart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=sqlalchemy.sql.func.now())
+    event_id = db.Column(db.Integer, db.ForeignKey(Event.id))
+    event = db.relationship(Event, backref=db.backref('votes', lazy='dynamic'))
+    session_id = db.Column(db.Integer, db.ForeignKey(Session.id))
+    sessions = db.relationship(Session,
+                               backref=db.backref('votes', lazy='dynamic'))
+    __table_args__ = (db.UniqueConstraint('event_id', 'session_id',
+                                          name='uq_event_session'),)
 
 
 # Views
 
 bp = flask.Blueprint('app', __name__)
+
+
+@bp.before_request
+def init_session():
+    if 'id' not in flask.session:
+        sess = Session()
+        db.session.add(sess)
+        db.session.commit()
+        flask.session['id'] = sess.id
+    if 'csrf' not in flask.session:
+        flask.session['csrf'] = base64.urlsafe_b64encode(os.urandom(30))
+
+
+def csrf(fn):
+    @functools.wraps(fn)
+    def wrapped(*args, **kwargs):
+        if not werkzeug.security.safe_str_cmp(flask.request.args['token'],
+                                              flask.session['csrf']):
+            raise BadRequest('Missing CSRF token')
+        return fn(*args, **kwargs)
+    return wrapped
 
 
 @bp.route('/')
@@ -47,7 +92,24 @@ def events():
 @bp.route('/events/<int:id>')
 def event_page(id):
     event = Event.query.filter(Event.id == id).first_or_404()
-    return flask.render_template('pages/event.html', event=event)
+    hearted = (Heart.query.filter(Heart.event == event,
+                                  Heart.session_id == flask.session['id'])
+                    .count() != 0)
+    return flask.render_template('pages/event.html', event=event,
+                                 hearted=hearted)
+
+
+@bp.route('/events/<int:id>/heart')
+@csrf
+def heart(id):
+    event = Event.query.filter(Event.id == id).first_or_404()
+    try:
+        h = Heart(event=event, session_id=flask.session['id'])
+        db.session.add(h)
+        db.session.commit()
+        return flask.Response(status=204)
+    except sqlalchemy.exc.IntegrityError:
+        raise werkzeug.exceptions.Conflict('You have already voted')
 
 
 @bp.route('/admin', methods=['GET', 'POST'])
@@ -65,15 +127,16 @@ def admin():
         try:
             for event in Event.query.all():
                 try:
-                    populate_from_form(event, flask.request.form, suffix)
+                    populate_from_form(event, flask.request.form, event.id)
                     db.session.add(event)
                 except KeyError:
                     # The event was removed
                     db.session.remove(event)
-            for new_suffix in flask.request.form.get('new', '').split(','):
-                event = Event()
-                populate_from_form(event, flask.request.form, new_suffix)
-                db.session.add(event)
+            if flask.request.form.get('new', ''):
+                for new_suffix in flask.request.form['new'].split(','):
+                    event = Event()
+                    populate_from_form(event, flask.request.form, new_suffix)
+                    db.session.add(event)
         except ValueError:
             flask.flash('You gave me bad data!')
             db.session.rollback()
@@ -116,7 +179,7 @@ def create_app():
             message = "It looks like you're lost"
         else:
             try:
-                message = exc.message
+                message = exc.description
             except AttributeError:
                 message = "Internal Server Error"
         return flask.render_template('pages/error.html', code=exc.code,
